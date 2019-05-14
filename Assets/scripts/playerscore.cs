81e99c69ed91aad385aec1010fb99b9e33bc1218
ktsng <22183062+ktsng@users.noreply.github.com>
2019-05-14 15:53:47 -0700

    unfeathered 2 release

--- Assets/scripts/playerscore.cs
+++ Assets/scripts/playerscore.cs
@@ -16,12 +16,25 @@ public class playerscore : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D trig) {
         if (trig.gameObject.name == "Owl") {
-            
+
             SceneManager.LoadScene(2);
         }
         if (trig.gameObject.name == "Worm") {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(4);
+        }
+        if (trig.gameObject.name == "Worm-tutorial-win") {
+            // go to hard mode
+            SceneManager.LoadScene(1);
         }
 
+        if (trig.gameObject.name == "owl-end") {
+
+            SceneManager.LoadScene(2);
+        }
+
+        if (trig.gameObject.name == "worm-end") {
+            // go to end
+            SceneManager.LoadScene(0);
+        }
     }
 }
